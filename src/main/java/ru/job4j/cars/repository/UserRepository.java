84e0a49176b4aee5c59e38ffd0f908c2e41e71a2
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-08-20 01:44:16 +0300

    2. HQL [#6874 #437898]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -15,105 +15,151 @@ public class UserRepository {
 
     /**
      * Сохранить в базе.
+     *
      * @param user пользователь.
      * @return пользователь с id.
      */
     public User create(User user) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
         return user;
     }
 
     /**
      * Обновить в базе пользователя.
+     *
      * @param user пользователь.
      */
     public void update(User user) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(user);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     /**
      * Удалить пользователя по id.
+     *
      * @param userId ID
      */
     public void delete(int userId) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        User item = new User();
-        item.setId(userId);
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            User item = new User();
+            item.setId(userId);
+            session.delete(item);
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     /**
      * Список пользователь отсортированных по id.
+     *
      * @return список пользователей.
      */
     public List<User> findAllOrderById() {
         Session session = sf.openSession();
-        session.beginTransaction();
-        List<User> result = session.createQuery("from User order by id asc", User.class)
-                .list();
-        session.getTransaction().commit();
-        session.close();
+        List<User> result = null;
+        try {
+            session.beginTransaction();
+            result = session.createQuery("from User order by id asc", User.class)
+                    .list();
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
         return result;
     }
 
     /**
      * Найти пользователя по ID
+     *
      * @return пользователь.
      */
     public Optional<User> findById(int userId) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        Query<User> query = session.createQuery(
-                "from User as i where i.id = :fId", User.class);
-        query.setParameter("fId", userId);
-        Optional<User> result = query.uniqueResultOptional();
-        session.getTransaction().commit();
-        session.close();
+        Optional<User> result = null;
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User as i where i.id = :fId", User.class);
+            query.setParameter("fId", userId);
+            result = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
         return result;
     }
 
     /**
      * Список пользователей по login LIKE %key%
+     *
      * @param key key
      * @return список пользователей.
      */
     public List<User> findByLikeLogin(String key) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        Query<User> query = session.createQuery(
-                "from User as u where u.login like :fLogin", User.class);
-        query.setParameter("fLogin", key);
-        List<User> result = query.list();
-        session.getTransaction().commit();
-        session.close();
+        List<User> result = null;
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User as u where u.login like :fLogin", User.class);
+            query.setParameter("fLogin", key);
+            result = query.list();
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
         return result;
     }
 
     /**
      * Найти пользователя по login.
+     *
      * @param login login.
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        Query<User> query = session.createQuery(
-                "from User as u where u.login=:fLogin", User.class);
-        query.setParameter("fLogin", login);
-        Optional<User> result = query.uniqueResultOptional();
-        session.getTransaction().commit();
-        session.close();
+        Optional<User> result = null;
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User as u where u.login=:fLogin", User.class);
+            query.setParameter("fLogin", login);
+            result = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
         return result;
     }
 }
